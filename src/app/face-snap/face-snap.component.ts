57b7e8e60bf063ed6dfcba1f13978fdbaeb72d8d
Lauffret <auffretlea@orange.fr>
2023-07-29 20:26:04 +0200

    ajout du component snap-face

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+  snapped!: boolean;
+
+  ngOnInit() {
+    this.title = 'Archiblad';
+    this.description = 'Mon meilleur ami depuis tout petit !';
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.snapped = false;
+  }
+
+  onSnap(){
+    if(this.snapped){
+      this.snaps--;
+      this.snapped = false;
+    }else{
+      this.snaps++;
+      this.snapped = true;
+    }
+  }
+}
