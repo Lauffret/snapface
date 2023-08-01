9284feb27b400fe7b163de2544a796f82ed12bef
Lauffret <auffretlea@orange.fr>
2023-08-01 22:36:10 +0200

    modification du scss et ajout de service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from './models/face-snap.model';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  faceSnaps!: FaceSnap[];
-
-
+  
   ngOnInit() {
-    this.faceSnaps = [
-      {
-        title: 'Archiblad',
-        description: 'Mon meilleur ami depuis tout petit !',
-        createdDate: new Date(),
-        snaps: 6,
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        snapped: false
-      },
-      {
-        title: 'TeddyBear',
-        description: 'Mon ami depuis tout petit !',
-        createdDate: new Date(),
-        snaps: 3,
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        snapped: true,
-        location: 'la campagne'
-      },
-      {
-        title: 'Three Rock Mountain',
-        description: 'Un endroit magnifique pour les randonnées.',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-        createdDate: new Date(),
-        snapped: false,
-        snaps: 100,
-        location: 'la montagne'
-      },
-      {
-        title: 'Un bon repas',
-        description: 'Mmmh que c\'est bon !',
-        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-        createdDate: new Date(),
-        snaps: 29,
-        snapped: true
-      }
-    ];
   }
 
 }
